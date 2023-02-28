6896671ae8b4dff840659b44ac24e9ea3126fb8d
jnovais1 <jnovais@findes.org.br>
2023-02-28 16:02:30 -0300

    conexão com o banco no Index

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include_once('./database/conexao.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,15 +11,16 @@
     <title>Metal Coins</title>
 </head>
 <body>
-    <h2>Cadastrar usuário</h2>
-    <form method="POST" action="./src/controllers/UserController.php">
-        <label>Nome: </label>
+    <h1>Metal Coins</h1>
+    <h3>Novo usuário</h3>
+    <form name="signIn" method="POST" action="./src/controllers/user/UserController.php">
+        <label>Nome: </label></br>
         <input type="text" name="name" placeholder="Nome completo"></br>
         <br><label>E-mail: </label></br>
         <input type="email" name="email" placeholder="E-mail"></br>
         <br><label>Senha: </label></br>
         <input type="password" name="password" placeholder="No mínimo 8 caracteres"></br>
-        <br><input type="submit" value="Cadastre-se">
+        <br><input type="submit" value="Cadastre-se" name="createdUser">
 </form>
 </body>
 </html>
