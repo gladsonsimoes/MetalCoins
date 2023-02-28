3948fce0b4a9ea7628fc341c3eee2e1d148c9e7a
jnovais1 <jnovais@findes.org.br>
2023-02-28 16:02:02 -0300

    inserção de usuários no banco

---
+++ src/controllers/user/UserController.php
@@ -0,0 +1,19 @@
+<?php
+include_once('././database/conexao.php');
+
+$user = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+if (!empty($user['createdUser'])) {
+    var_dump($user);
+}
+if (empty($id_perfil)) {
+    $id_perfil = 1;
+}
+$queryUser = "INSERT INTO usuarios (id_perfil, nome, email, senha) 
+        VALUES ($id_perfil,
+            '" . $user['name'] . "',
+             '" . $user['email'] . "',
+              '" .$user['password']. "')";
+var_dump($queryUser);
+$signedUser = $conn->prepare($queryUser);
+$signedUser->execute();
