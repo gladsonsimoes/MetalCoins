4c81f27a5bca609c2c3512170b15cb24be504f76
jnovais1 <109621297+jnovais1@users.noreply.github.com>
2023-02-27 21:33:11 -0300

    changes on sql queries

--- database/metalcoins.sql
+++ database/metalcoins.sql
@@ -6,7 +6,7 @@ use metalcoin;
 CREATE TABLE usuario 
 ( 
  id INT PRIMARY KEY AUTO_INCREMENT,  
- idPerfil INT,  
+ id_perfil INT,  
  nome VARCHAR(45) NOT NULL,  
  email VARCHAR(45) NOT NULL,  
  senha VARCHAR(45) NOT NULL 
@@ -14,7 +14,7 @@ CREATE TABLE usuario
 
 CREATE TABLE perfil 
 ( 
- tipoDeUsuario INT PRIMARY KEY
+ tipo_de_usuario INT PRIMARY KEY
 ); 
 
 CREATE TABLE produto 
@@ -28,28 +28,28 @@ CREATE TABLE produto
 CREATE TABLE conta_corrente 
 ( 
  id INT PRIMARY KEY AUTO_INCREMENT,  
- idUsuario INT,  
+ id_usuario INT,  
  Saldo FLOAT NOT NULL,  
- Transacao VARCHAR(45) NOT NULL,  
- DataTransacao DATE NOT NULL  
+ transacao VARCHAR(45) NOT NULL,  
+ data_transacao DATE NOT NULL  
 ); 
 
 CREATE TABLE pedido 
 ( 
  id INT PRIMARY KEY AUTO_INCREMENT,  
- idProduto INT,  
- quantidadeProduto INT NOT NULL,  
- idUsuario INT,  
+ id_produto INT,  
+ id_usuario INT NOT NULL,  
+ quantidade_produto INT NOT NULL,  
  status VARCHAR(45) NOT NULL,  
- dataPedido DATE NOT NULL ,
- valorPedido FLOAT NOT NULL
+ data_pedido DATE NOT NULL,
+ valor_pedido FLOAT NOT NULL
 ); 
 
-ALTER TABLE usuario ADD FOREIGN KEY(idPerfil) REFERENCES perfil (tipoDeUsuario);
+ALTER TABLE usuario ADD FOREIGN KEY(id_perfil) REFERENCES perfil (tipo_de_usuario);
 
-ALTER TABLE conta_corrente ADD FOREIGN KEY(idUsuario) REFERENCES usuario (id);
+ALTER TABLE conta_corrente ADD FOREIGN KEY(id_usuario) REFERENCES usuario (id);
 
-ALTER TABLE pedido ADD FOREIGN KEY(idProduto) REFERENCES produto (id);
+ALTER TABLE pedido ADD FOREIGN KEY(id_produto) REFERENCES produto (id);
 
-ALTER TABLE pedido ADD FOREIGN KEY(idUsuario) REFERENCES usuario (id);   
+ALTER TABLE pedido ADD FOREIGN KEY(id_usuario) REFERENCES usuario (id);   
    
