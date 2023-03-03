1a78e9e95b4fd1614c8c4c704c3fb94158a1062b
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-03 12:29:49 -0300

    Adicionei algumas coisas

--- Cadastro.php
+++ Cadastro.php
@@ -1,5 +1,6 @@
 <?php
 include_once('./database/conexao.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -17,31 +18,31 @@ include_once('./database/conexao.php');
     <nav>
         <div class="topnav">
             <a href="index.html">Home</a>
-          </div>
+        </div>
     </nav>
 
-    <!-- Conteudo da página -->   
+    <!-- Conteudo da página -->
     <h1>Metal Coins</h1>
     <h3>Cadastro Usuário</h3>
     <div class="container">
-        <form name="signIn" method="POST" action="./src/controllers/user/UserController.php">
+        <form name="signIn" method="post" action="./src/controllers/user/UserController.php">
             <label>Nome: </label>
             <br>
-            <input type="text" name="name" placeholder="Nome completo" required/>
+            <input type="text" name="name" placeholder="Nome completo" required />
             <br>
             <br><label>E-mail: </label>
             <br>
-            <input type="email" name="email" placeholder="E-mail" required/>
+            <input type="email" name="email" placeholder="E-mail" required />
             <br>
             <br><label>Senha: </label>
             <br>
-            <input type="password" name="password" placeholder="No mínimo 8 caracteres" minlength="8" required/>
+            <input type="password" name="password" placeholder="No mínimo 8 caracteres" minlength="8" required />
             <br><br>
             <b> Ao cadastrar você irá concordar com a nossa <a href="">politica de dados</a> </b>
             <br><br>
             <input type="submit" value="Cadastre-se" name="createdUser">
         </form>
-        <h3>Já tem Cadastro?, <a href="login.php">Clique para o Login</a></h3>
+        <h3>Já tem Cadastro? <a href="login.php">Clique para o Login</a></h3>
     </div>
 </body>
 
