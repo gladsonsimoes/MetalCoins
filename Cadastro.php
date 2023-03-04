1b11dd9a0f5b1ee6ecb9d638e40d004f9d34c51e
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-04 18:48:44 -0300

    Alteracoes no visual

--- Cadastro.php
+++ Cadastro.php
@@ -10,40 +10,57 @@ include_once('./database/conexao.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Cadastro MetalCoins</title>
 </head>
 
 <body>
-    <!-- NavBar -->
-    <nav>
-        <div class="topnav">
-            <a href="index.html">Home</a>
-        </div>
-    </nav>
+    <header>
+        <!-- NavBar -->
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">MetalCoins</a>
+        </nav>
+    </header>
+    <br>
 
     <!-- Conteudo da página -->
-    <h1>Metal Coins</h1>
-    <h3>Cadastro Usuário</h3>
     <div class="container">
+        <h3>Cadastro Usuário</h3>
+        <br>
         <form name="signIn" method="post" action="./src/controllers/user/UserController.php">
-            <label>Nome: </label>
+            <div class="form-group form-row">
+                <label>Nome:</label>
+                <div class="col-4">
+                    <input type="text" name="name" class="form-control" placeholder="Nome completo" required
+                        autocomplete="off" />
+                </div>
+            </div>
             <br>
-            <input type="text" name="name" placeholder="Nome completo" required />
+            <div class="form-group form-row">
+                <label>E-mail:</label>
+                <div class="col-4">
+                    <input type="email" name="email" class="form-control" placeholder="Digite seu E-mail" required
+                        autocomplete="off" />
+                </div>
+            </div>
             <br>
-            <br><label>E-mail: </label>
+            <div class="form-group form-row">
+                <label>Senha:</label>
+                <div class="col-4">
+                    <input type="password" name="password" class="form-control" placeholder="Crie sua senha"
+                        minlength="8" required />
+                </div>
+            </div>
             <br>
-            <input type="email" name="email" placeholder="E-mail" required />
-            <br>
-            <br><label>Senha: </label>
-            <br>
-            <input type="password" name="password" placeholder="No mínimo 8 caracteres" minlength="8" required />
-            <br><br>
             <b> Ao cadastrar você irá concordar com a nossa <a href="">politica de dados</a> </b>
             <br><br>
             <input type="submit" value="Cadastre-se" name="createdUser">
         </form>
-        <h3>Já tem Cadastro? <a href="login.php">Clique para o Login</a></h3>
-    </div>
+        <div>
+            <br>
+            <h3>Já tem Cadastro? <a href="login.php">Clique para o Login</a></h3>
+        </div>
 </body>
 
 </html>
