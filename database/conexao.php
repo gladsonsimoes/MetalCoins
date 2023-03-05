5c32a3d41a892d37dd1a06e9f78ac3c30f63fe2a
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-05 10:39:32 -0300

    Erro de mesclagem corrigida

--- database/conexao.php
+++ database/conexao.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $host = "localhost";
 $user = "root";
@@ -12,21 +11,4 @@ $conn = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $user, $p
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
-
-=======
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "metalcoin";
-$port = 3306;
-
-
-try {
-$conn = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $user, $pass);
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
-
->>>>>>> 5b48719c84571b687d309dc60e2e4a5f9116fe0d
 ?>
