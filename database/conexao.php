274e4e327846bac2b7fbd05ec45bd777001490b5
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-05 10:34:08 -0300

    Melhorias na conexao

--- database/conexao.php
+++ database/conexao.php
@@ -1,10 +1,13 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "123456";
+$pass = "";
 $dbname = "metalcoin";
+$port = 3306;
+
+
 try {
-$conn = new PDO('mysql:host=localhost;dbname=metalcoin', $user, $pass);
+$conn = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $user, $pass);
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
