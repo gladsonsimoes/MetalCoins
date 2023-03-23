500e3667d90cf3dba164345d73dbf3e4ecca81c4
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-23 19:57:10 -0300

    Remocao de comentario inuteis

--- src/controllers/user/UserController.php
+++ src/controllers/user/UserController.php
@@ -6,42 +6,40 @@ $user = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 if (empty($user['createdUse'])) {
 
-    //validacao de formulario
-    $empty_input = false;
+  //validacao de formulario
+  $empty_input = false;
 
-    $user = array_map('trim', $user);
-    if (in_array("", $user)) {
-        $empty_input = true;
-        echo "<script type='text/javascript'>
+  $user = array_map('trim', $user);
+  if (in_array("", $user)) {
+    $empty_input = true;
+    echo "<script type='text/javascript'>
         alert('Digite os campos corretamente e tente novamente!');
         window.location.href='../../../Cadastro.php';
       </script>";
-        // echo "<p style='color: red;'> Digite os campos corretamente </p>";
-    } else if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
-        $empty_input = true;
-        echo "<script type='text/javascript'>
+  } else if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
+    $empty_input = true;
+    echo "<script type='text/javascript'>
         alert('Digite um E-mail válido !');
         window.location.href='../../../Cadastro.php';
       </script>";
-        // echo "<p style='color: red;'> Digite um E-mail válido </p>";
-    }
+  }
 
-    //se empty_input for false , inserir dados no banco de dados
-    if (!$empty_input) {
-        $queryUser = " INSERT INTO usuarios (nome, email, senha) VALUES 
-              ('" . $user['name'] .
-            "','" . $user['email'] .
-            "','" . $user['password'] . "'); ";
+  //se empty_input for false , inserir dados no banco de dados
+  if (!$empty_input) {
+    $queryUser = " INSERT INTO usuarios (nome, email, senha) VALUES 
+('" . $user['name'] .
+      "','" . $user['email'] .
+      "','" . $user['password'] . "'); ";
 
-        $signedUser = $conn->prepare($queryUser);
+    $signedUser = $conn->prepare($queryUser);
 
-        $signedUser->execute();
+    $signedUser->execute();
 
-        echo "<script type='text/javascript'>
+    echo "<script type='text/javascript'>
           alert('Cadastro Realizado com Sucesso! ');
           window.location.href='../../../index.php';
         </script>";
-    }
+  }
 }
 
 //validacao para conta admin e cliente
