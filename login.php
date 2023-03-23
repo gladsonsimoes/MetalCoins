349b346816946ca2c13081e35d3b7a721a6105d4
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-23 20:18:22 -0300

    removi codigo bugado de alteracao

--- login.php
+++ login.php
@@ -35,11 +35,9 @@ include_once('./database/conexao.php');
             <br>
             <div class="form-group form-row">
                 <div class="col-4">
-<<<<<<< HEAD
-                    <input type="password" name="password" class="form-control" placeholder="Digite sua senha"
-=======
+
                     <input type="password" name="password" class=" form-control rounded-pill" placeholder="Digite sua senha"
->>>>>>> e3b4842e108ffdc55ae1655de34a8886726121e3
+
                         minlength="8" required autocomplete="on" />
                 </div>
             </div>
