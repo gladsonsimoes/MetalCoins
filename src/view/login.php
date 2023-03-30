57ea36c9f81614f06b1eeeec3eda77f143684710
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2023-03-30 19:46:22 -0300

    Nova Versao do Front end

--- src/view/login.php
+++ src/view/login.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../../database/conexao.php');
+include_once('./database/conexao.php');
 
 ?>
 
@@ -10,43 +10,71 @@ include_once('../../database/conexao.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="styles/login.css">
+        <!-- <link type="text/css" rel="stylesheet" href="css/metalcoins.css"> -->
     <title>Login MetalCoins</title>
+
+   
+    </style>
 </head>
 
-<body>
+<body style="background: linear-gradient(123.7deg, #7236AD -33.13%, #313C59 111.98%); height: 20px;">
+
     <br>
-    <!-- Conteudo da Página -->
+
     <div class="container" align="center">
         <div id="tituloMetalCoins">
-            <h1>Metal Coins</h1>
+            <h1 id="titulo">Metal Coins</h1>
         </div>
-        <br>
-        <h3>Fazer Login</h3>
-        <br>
-        <form name="signIn" method="POST" action="../controllers/user/LoginConnect.php">
-            <div class="form-group form-row">
-                <div class="col-4">
-                    <input type="email" name="email" class="form-control rounded-pill " placeholder="Digite seu E-mail " required
-                        autocomplete="on" />
-                </div>
-            </div>
-            <br>
-            <div class="form-group form-row">
-                <div class="col-4">
-                    <input type="password" name="password" class=" form-control rounded-pill" placeholder="Digite sua senha"
+        
 
-                        minlength="8" required autocomplete="on" />
+        <div class="imagem">
+        </div>
+    
+
+        <br>
+        <br>
+
+        <div id="borda1" style="background: linear-gradient(180deg, #6E3AC2 0%, rgba(217, 217, 217, 0) 107.71%);" class="container">
+
+        <br>
+    <br>
+            <div class="form-group form-row">
+                <div class="col-9">
+                    <input type="email" name="email" class="form-control rounded-pill ex3" placeholder="E-mail:" required
+                        autocomplete="off" />
                 </div>
             </div>
             <br>
-            <input type="submit" value="Entrar" name="loginUser">
+            <div class="form-group form-row">
+                <div class="col-9">
+                    <input type="password" name="password" class="form-control rounded-pill ex1" placeholder="Senha:"
+                        minlength="8" required />
+                </div>
+            </div>
+            
+           <a href="conta.php"><input style="background-color: #332E51;" type="submit" value="Login" name="createdUser" class="btn btn-dark btn-lg rounded-pill ex"></a>
         </form>
         <br>
-        <h4>Não tem uma conta? <a href="Cadastro.php">Cadastre-se</a></h4>
-        <b><a href="">Esqueceu a senha? </a></b>
-    </div>
+        </div>
+    
+
+        <br>
+        <div>
+            <h4 class="form-check-label fn">Ainda não é cadastrado? <a href="cadastro.php">register</a></h4>
+        </div>
+        <script type="text/javascript" src="vanilla-tilt.js"></script>
+        <script>VanillaTilt.init(document.querySelectorAll(".container"));</script>
+
 </body>
 
 </html>
