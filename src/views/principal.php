3f313501044d457b61a1ebaa39315a4b60b12b33
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2023-03-30 21:44:10 -0300

    adicionei pastas js, img e views (para decidir se usaremos antiga ou nova)

---
+++ src/views/principal.php
@@ -0,0 +1,36 @@
+<?php
+include_once('../../database/conexao.php');
+
+?>
+
+<!DOCTYPE >
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Login MetalCoins</title>
+
+    <style>
+        div.borda1 {
+    border-top-width: 3px;
+    border-top-style: solid;
+    border-top-color: #000;
+}
+
+div.borda1 {
+    border-top: 45px solid blue;
+}
+
+    </style>
+           
+</head>
+
+<body>
+    <div class="borda1"></div>
+    
+</body>
+
+</>
