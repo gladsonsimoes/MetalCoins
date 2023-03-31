58f85056339e527cd7a5a0dc01668798f33dda41
jnovais1 <jnovais@findes.org.br>
2023-03-31 15:41:09 -0300

    incluindo o backend na pagina de conta

--- src/views/conta.php
+++ src/views/conta.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+include_once('../controllers/clientUser/conta/conta.php');
 
 ?>
 <!DOCTYPE html>
@@ -10,52 +11,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../styles/conta.css">
-        <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/conta.css">
+    <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
 
-        <!-- <link type="text/css" rel="stylesheet" href="css/metalcoins.css"> -->
+    <!-- <link type="text/css" rel="stylesheet" href="css/metalcoins.css"> -->
     <title>Conta Metalcoins</title>
-   
-   
+
+
 </head>
 
-<body  style="background: #2E0A5B;">
-        <h3 class="tituloU">Olá, João Lindão</h3>
-        <span>
-            <button class="iconeUsuario"></button>
-        </span>
-     
-       <div class="borda1">
+<body style="background: #2E0A5B;">
+    <h3 class="tituloU">Olá, João Lindão</h3>
+    <span>
+        <button class="iconeUsuario"></button>
+    </span>
+
+    <div class="borda1">
         <h1 class="saldoT">Conta</h1>
         <h1 class="saldoD">Saldo Disponivel</h1>
         <h1 class="valorT">10.000</h1>
     </div>
-
-        <div class="borda2">
-
+    <div class="borda2">
+    </div>
+    <div class="borda3">
     </div>
 
-        <div class="borda3">
-            
-    </div>
-    
-       <script type="text/javascript" src="../js/vanilla-tilt.js"></script>
+    <script type="text/javascript" src="../js/vanilla-tilt.js"></script>
     <script>
         VanillaTilt.init(document.querySelectorAll(".borda1"));
         VanillaTilt.init(document.querySelectorAll(".borda2"));
         VanillaTilt.init(document.querySelectorAll(".borda3"));
-
-    VanillaTilt.init(document.querySelectorAll(".iconeUsuario"));
-
+        VanillaTilt.init(document.querySelectorAll(".iconeUsuario"));
     </script>
 
-    
+
 </body>
 
 </html>
