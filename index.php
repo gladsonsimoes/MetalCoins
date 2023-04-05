f7cba75ec82a4251173f1b3a01d956a71443e513
jnovais1 <jnovais@findes.org.br>
2023-04-05 15:41:12 -0300

    index agora é o login

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-include_once('database\conexao.php');
-$login_cookie = $_COOKIE['login'];
-    if(isset($login_cookie)){
-      echo"Bem-Vindo, $login_cookie <br>";
-      echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
-    }else{
-      echo"Bem-Vindo, convidado <br>";
-      echo"Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
-      echo"<br><a href='src/views/login.php'>Faça Login</a> Para ler o conteúdo";
-    }
+include_once('database/conexao.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -20,32 +12,70 @@ $login_cookie = $_COOKIE['login'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>MetalCoins</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="src/styles/login.css">
+        <!-- <link type="text/css" rel="stylesheet" href="css/metalcoins.css"> -->
+    <title>Login MetalCoins</title>
+
+   
+    </style>
 </head>
 
-<body>
-    <!-- NavBar -->
-     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">MetalCoins</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <a class="nav-item nav-link" href="src/views/login.php">Acesse sua conta</a>
-              </div>
-            </div>
-          </nav>
-    </header>
+<body style="background: linear-gradient(123.7deg, #7236AD -33.13%, #313C59 111.98%); height: 20px;">
+
     <br>
 
-    <!-- Conteudo da página -->   
-  
-    <h3>Página de Navegação</h3>
-    <div class="container">
-         
-    </div>
+    <div class="container" align="center">
+        <div id="tituloMetalCoins">
+            <h1 id="titulo">Metal Coins</h1>
+        </div>
+        
+
+        <div class="imagem">
+        </div>
+    
+
+        <br>
+        <br>
+        <div id="borda1" style="background: linear-gradient(180deg, #6E3AC2 0%, rgba(217, 217, 217, 0) 107.71%);" class="container">
+
+        <br>
+    <br>
+            <form id="signIn" method="POST" action="src/controllers/user/LoginConnect.php">
+            <div class="form-group form-row">
+                <div class="col-9">
+                    <input type="email" name="email" class="form-control rounded-pill ex3" placeholder="E-mail:" required
+                        autocomplete="off" />
+                </div>
+            </div>
+            <br>
+            <div class="form-group form-row">
+                <div class="col-9">
+                    <input type="password" name="password" class="form-control rounded-pill ex1" placeholder="Senha:"
+                        minlength="8" required />
+                </div>
+            </div>
+            
+           <a href="conta.php"><input style="background-color: #332E51;" type="submit" value="Login" name="loginUser" class="btn btn-dark btn-lg rounded-pill ex"></a>
+            </form>
+            
+        </form>
+        <br>
+        </div>
+    
+
+        <br>
+        <div>
+            <h4 class="form-check-label fn">Ainda não é cadastrado? <a href="src/views/cadastro.php">Registrar</a></h4>
+        </div>
+        
+
 </body>
 
 </html>
