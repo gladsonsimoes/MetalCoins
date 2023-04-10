618303ad0704c9eb2830f1e40c6fabfa51c7c5e7
jnovais1 <jnovais@findes.org.br>
2023-04-10 10:37:17 -0300

    Iniciando trabalhos backend de produtos

---
+++ src/controllers/products/productsController.php
@@ -0,0 +1,66 @@
+<?php
+include_once('../../../database/conexao.php');
+
+function indexProducts()
+{
+    global $conn;
+    $empty_input = false;
+
+    $query = "SELECT * FROM produto";
+    $products = $conn->prepare($query);
+    $products->execute();
+
+    return $products;
+}
+
+function getProductById($id_product)
+{
+    global $conn;
+    $empty_input = false;
+
+    $query = "SELECT * FROM produto WHERE id='" . $id_product . "'";
+    $checkingProduct = $conn->prepare($query);
+    $checkingProduct->execute();
+    $rows = $checkingProduct->rowCount();
+
+    if ($rows > 0) {
+        return $rows;
+    }
+}
+
+function store($product)
+{
+    global $conn;
+    $empty_input = false;
+
+    $query = "SELECT * FROM produto WHERE nome='" . $product['nome'] . "'";
+    $checkingProduct = $conn->prepare($query);
+    $checkingProduct->execute();
+    $rows = $checkingProduct->rowCount();
+
+    if ($rows > 0) {
+        echo ("<script type='text/javascript'>
+        alert('Produto já cadastrado! ');
+        </script>");
+    } 
+    else {
+
+        $query = "INSERT INTO `produto`
+        (`nome`, `descricao`, `preco`, `estoque`)
+        VALUES
+        ('{$product['nome']}',
+        '{$product['descricao']}',
+        '{$product['preco']}',
+        '{$product['estoque']}');";
+
+        $creatingProduct = $conn->prepare($query);
+        $creatingProduct->execute();
+        $product = $creatingProduct->rowCount();
+        if ($product > 0) {
+            echo ("<script type='text/javascript'>
+            alert('Produto cadastrado com sucesso!');
+            </script>");
+        }
+    }
+}
+?>
