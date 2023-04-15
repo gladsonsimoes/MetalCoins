5bf8e66bb9891c1ffe31e2acdb9eec6fbc734ef5
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-04-15 16:36:33 -0300

    Alguns ajustes da home admin

--- src/views/admin.php
+++ src/views/admin.php
@@ -1,11 +1,12 @@
-<?php 
- include('../controllers/Acess/protectedAdmin.php');
- include('../controllers/Acess/protectedAdmin.php');
+<?php
+include('../controllers/Acess/protectedAdmin.php');
+include('../controllers/Acess/protectedAdmin.php');
 
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,8 +27,9 @@
 
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
 </head>
+
 <body id="body">
-    
+
     <header>
         <div class="icon__menu">
             <i class="fas fa-bars" id="btn_open"></i>
@@ -37,65 +39,62 @@
         <form action="">
             <div class="form-group form-row">
                 <input type="text" name="name" class="form-control rounded-pill ex" required autocomplete="off" />
-    
+
             </div>
 
-    <div style="background: #6E3AC2;" class="menu__side" id="menu_side">
+            <div style="background: #6E3AC2;" class="menu__side" id="menu_side">
 
-        <div class="name__page">
-            <i class="fa-solid fa-coins"></i>
-            <h4 id="sub-titulo">Metalcoins</h4>
-        </div>
-
-        <div class="options__menu hp">	
-
-            <a  class="selected">
-                <div class="option">
-                    <i  class="fas fa-home" title="Inicio"></i>
-                    <h4>Inicio</h4>
+                <div class="name__page">
+                    <i class="fa-solid fa-coins"></i>
+                    <h4 id="sub-titulo">Metalcoins</h4>
                 </div>
-            </a>
 
-            <a href="conta.php"> 
-                <div class="option">
-                    <i class="fas fa-user" title="Conta"></i>
-                    <h4>Conta</h4>
+                <div class="options__menu hp">
+                    <a class="selected">
+                        <div class="option">
+                            <i class="fas fa-home" title="Inicio"></i>
+                            <h4>Inicio</h4>
+                        </div>
+                    </a>
+
+                    <a href="conta.php">
+                        <div class="option">
+                            <i class="fas fa-user" title="Conta"></i>
+                            <h4>Conta</h4>
+                        </div>
+                    </a>
+
+                    <a href="#">
+                        <div class="option">
+                            <i class="far fa-id-badge" title="Contacto"></i>
+                            <h4>Contato</h4>
+                        </div>
+                    </a>
+
+                    <a href="CadastroProduto.php">
+                        <div class="option">
+                            <i class="fas fa-edit" aria-hidden="true" title="Gerenciar Produtos"></i>
+                            <h4>Gerenciar produtos</h4>
+                        </div>
+                    </a>
+
+                    <a href="tabela.php">
+                        <div class="option">
+                            <i class="fas fa-edit" aria-hidden="true" title="Gerenciar usuários"></i>
+                            <h4>Gerenciar usuários</h4>
+                        </div>
+                    </a>
+
+                    <a href="../controllers/Acess/logout.php">
+                        <div id="sair" class="option">
+                            <i class="fa fa-sign-out" aria-hidden="true" title="Exit"></i>
+                            <h4>Sair</h4>
+                        </div>
+                    </a>
                 </div>
-            </a>
+            </div>
 
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-id-badge" title="Contacto"></i>
-                    <h4>Contato</h4>
-                </div>
-            </a>
-
-            <a href="CadastroProduto.php">
-                <div class="option">
-                    <i class="fas fa-edit" aria-hidden="true" title="Gerenciar Produtos"></i>
-                    <h4>Gerenciar produtos</h4>
-                </div>
-            </a>
-
-            <a href="tabela.php">
-                <div class="option">
-                    <i class="fas fa-edit" aria-hidden="true" title="Gerenciar usuários"></i>
-                    <h4>Gerenciar usuários</h4>
-                </div>
-            </a>
-
-            <a href="../controllers/Acess/logout.php">
-                <div id="sair" class="option">
-                    <i class="fa fa-sign-out" aria-hidden="true" title="Exit"></i>
-                    <h4>Sair</h4>
-                </div>
-            </a>
-
-        </div>
-
-    </div>
-
-    <main>
+         
         </form>
         <div class="Produtos">
             <div class="produto1">
@@ -104,9 +103,8 @@
                     <img id="camera" src="../img/camera.png" alt="camera">
                 </div>
                 <a href=""><span id="vermais">Ver mais</span></a>
-        
             </div>
-        
+
             <div class="produto2">
                 <h1 id="p2">Iphone 11 Pro</h1>
                 <div class="quadrado2">
@@ -114,7 +112,7 @@
                 </div>
                 <span id="vermais">Ver mais</span>
             </div>
-        
+
             <div class="produto3">
                 <h1 id="p3">HD SSD 960GB Kingston 2,5 sata</h1>
                 <div class="quadrado3">
@@ -122,7 +120,7 @@
                 </div>
                 <span id="vermais">Ver mais</span>
             </div>
-        
+
             <div class="produto4">
                 <h1 id="p4">Notebook Deel Latitude 3420 14 Polegadas</h1>
                 <div class="quadrado4">
@@ -130,7 +128,7 @@
                 </div>
                 <span id="vermais">Ver mais</span>
             </div>
-        
+
             <div class="produto5">
                 <h1 id="p5">Paçoca Dacolônia Rolha 210g</h1>
                 <div class="quadrado5">
@@ -138,7 +136,7 @@
                 </div>
                 <span id="vermais">Ver mais</span>
             </div>
-        
+
             <div class="produto6">
                 <h1 id="p6">NIke Air Force Shoes</h1>
                 <div class="quadrado6">
@@ -146,46 +144,47 @@
                 </div>
                 <span id="vermais">Ver mais</span>
             </div>
-        
+
         </div>
-    </main>
+        </main>
 
-    <script src="../js/script.js"></script>
+        <script src="../js/script.js"></script>
 
-    <script type="text/javascript" src="../js/vanilla-tilt.js"></script>
-    <script>
-        VanillaTilt.init(document.querySelectorAll(".produto1"));
-        VanillaTilt.init(document.querySelectorAll(".quadrado1"));
+        <script type="text/javascript" src="../js/vanilla-tilt.js"></script>
+        <script>
+            VanillaTilt.init(document.querySelectorAll(".produto1"));
+            VanillaTilt.init(document.querySelectorAll(".quadrado1"));
 
-        VanillaTilt.init(document.querySelectorAll(".produto2"));
-        VanillaTilt.init(document.querySelectorAll(".quadrado2"));
+            VanillaTilt.init(document.querySelectorAll(".produto2"));
+            VanillaTilt.init(document.querySelectorAll(".quadrado2"));
 
-        VanillaTilt.init(document.querySelectorAll(".produto3"));
-        VanillaTilt.init(document.querySelectorAll(".quadrado3"));
+            VanillaTilt.init(document.querySelectorAll(".produto3"));
+            VanillaTilt.init(document.querySelectorAll(".quadrado3"));
 
-        VanillaTilt.init(document.querySelectorAll(".produto4"));
-        VanillaTilt.init(document.querySelectorAll(".quadrado4"));
+            VanillaTilt.init(document.querySelectorAll(".produto4"));
+            VanillaTilt.init(document.querySelectorAll(".quadrado4"));
 
-        VanillaTilt.init(document.querySelectorAll(".produto5"));
-        VanillaTilt.init(document.querySelectorAll(".quadrado5"));
+            VanillaTilt.init(document.querySelectorAll(".produto5"));
+            VanillaTilt.init(document.querySelectorAll(".quadrado5"));
 
-        VanillaTilt.init(document.querySelectorAll(".produto6"));
-        VanillaTilt.init(document.querySelectorAll(".quadrado6"));
+            VanillaTilt.init(document.querySelectorAll(".produto6"));
+            VanillaTilt.init(document.querySelectorAll(".quadrado6"));
 
-        VanillaTilt.init(document.querySelectorAll("#camera"));
-        VanillaTilt.init(document.querySelectorAll("#iphone"));
-        VanillaTilt.init(document.querySelectorAll("#ssd"));
-        VanillaTilt.init(document.querySelectorAll("#notebook"));
-        VanillaTilt.init(document.querySelectorAll("#pacoca"));
-        VanillaTilt.init(document.querySelectorAll("#nike"));
+            VanillaTilt.init(document.querySelectorAll("#camera"));
+            VanillaTilt.init(document.querySelectorAll("#iphone"));
+            VanillaTilt.init(document.querySelectorAll("#ssd"));
+            VanillaTilt.init(document.querySelectorAll("#notebook"));
+            VanillaTilt.init(document.querySelectorAll("#pacoca"));
+            VanillaTilt.init(document.querySelectorAll("#nike"));
 
-        VanillaTilt.init(document.querySelectorAll(".iconeUsuario"));
-
-
+            VanillaTilt.init(document.querySelectorAll(".iconeUsuario"));
 
 
 
 
-    </script>
+
+
+        </script>
 </body>
+
 </html>
