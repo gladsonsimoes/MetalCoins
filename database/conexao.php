347e1f52b30d81c120139f0f5dd784c410803ea9
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-04-15 17:42:15 -0300

    Mensagem de erro de conexao maior

--- database/conexao.php
+++ database/conexao.php
@@ -1,15 +1,24 @@
 <?php
+
+$dbname = "metalcoin";
 $host = "localhost";
 $user = "root";
 $pass = "123456";
-$dbname = "metalcoin";
 $port = 3306;
-
 
 try {
 $conn = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $user, $pass);
 } catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
+    echo '<br>
+        <div align="center">
+         <h1 style="color: red;"> ERROR: <br></h1>
+         <h2>' . $e->getMessage(). '</h2>
+        </div>
+   <br>';
+
+//[2002] -> Port ou Host Errado
+//[1045] -> User ou Pass Errado (Pass : talvez seu servidor não tenha senha!)
+//[1049] -> dbname Errado
 }
 
 ?>
