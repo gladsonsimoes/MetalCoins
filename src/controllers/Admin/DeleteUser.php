f8b719a23fca834889c0a9a370975c89060e99f3
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-04-16 17:01:17 -0300

    Retirei o id_perfil pois a tabela só tem cliente

--- src/controllers/Admin/DeleteUser.php
+++ src/controllers/Admin/DeleteUser.php
@@ -2,31 +2,30 @@
 include('../../../database/conexao.php');
 
 $id = $_GET['id'];
-$id_perfil = $_GET['id_perfil'];
 
-if ($id_perfil == 2) {
+if ($id) {
+        $selectSQL = $conn->prepare("SELECT * FROM usuarios WHERE id=$id");
+        $selectSQL->execute();
 
-    $selectSQL = $conn->prepare("SELECT * FROM usuarios WHERE id=$id");
-    $selectSQL->execute();
+        $count_rows = $selectSQL->rowCount();
 
-    $count_rows = $selectSQL->rowCount();
-
-    if ($count_rows > 0) {
-        $deleteSQL = $conn->prepare("DELETE FROM usuarios WHERE id=$id");
-        $deleteSQL->execute();
-        if ($deleteSQL) {
-            echo "<script>
+        if ($count_rows > 0) {
+            $deleteSQL = $conn->prepare("DELETE FROM usuarios WHERE id=$id");
+            $deleteSQL->execute();
+            if ($deleteSQL) {
+                echo "<script>
             alert('Usuário Excluído!');
             window.location.href='../../views/tabela.php';
              </script>";
+            }
+        } else {
+            die("Usuario nao existe!");
         }
-    } else {
-        die("Usuario nao existe!");
-    }
-} else if ($id_perfil == 1) {
-    echo "<script>
-    alert('Você não pode excluir um administrador!');
-    window.location.href='../../views/tabela.php';
-     </script>";
 }
+// else if ($id_perfil == 1) {
+//     echo "<script>
+//     alert('Você não pode excluir um administrador!');
+//     window.location.href='../../views/tabela.php';
+//      </script>";
+// }
 ?>
